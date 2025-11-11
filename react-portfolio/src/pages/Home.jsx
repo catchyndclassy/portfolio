6f3fe46987eb818dbdc6578a6142a659ba2427fe
john <johnsonnithish@gmail.com>
2025-11-11 09:21:09 +0530

    Stabe Backgrounf

--- react-portfolio/src/pages/Home.jsx
+++ react-portfolio/src/pages/Home.jsx
@@ -1,19 +1,4 @@
-import React from "react";
 import ColorBlend from "../components/backgrounds/ColorBlends";
-import StaggeredMenu from "../components/menus/StaggeredMenu";
-
-const menuItems = [
-  { label: 'Home', ariaLabel: 'Go to home page', link: '/' },
-  { label: 'About', ariaLabel: 'Learn about us', link: '/about' },
-  { label: 'Services', ariaLabel: 'View our services', link: '/services' },
-  { label: 'Contact', ariaLabel: 'Get in touch', link: '/contact' }
-];
-
-const socialItems = [
-  { label: 'Twitter', link: 'https://twitter.com' },
-  { label: 'GitHub', link: 'https://github.com' },
-  { label: 'LinkedIn', link: 'https://linkedin.com' }
-];
 
 export default function Home() {
   return (
@@ -30,17 +15,6 @@ export default function Home() {
           parallax={0.5}
           noise={0.1}
           transparent={false}
-        />
-        <StaggeredMenu
-          isFixed={true}
-          position="right"
-          items={menuItems}
-          socialItems={socialItems}
-          displaySocials={true}
-          displayItemNumbering={true}
-          // logoUrl is already defaulted to /src/assets/CatchyndClassy_LOGO.jpg,
-          // but you can override it here if needed
-          // logoUrl="/src/assets/CatchyndClassy_LOGO.jpg"
         />
       </div>
     </>
