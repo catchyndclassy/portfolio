29eb679d608f498798feb29f1017e00ea6b14751
john <johnsonnithish@gmail.com>
2025-11-11 10:24:21 +0530

    Content- Initial

--- react-portfolio/src/pages/Home.jsx
+++ react-portfolio/src/pages/Home.jsx
@@ -1,22 +1,32 @@
+import React from "react";
 import ColorBlend from "../components/backgrounds/ColorBlends";
+import "./Home.css";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-span">
-        <ColorBlend
-          colors={["#d12c16ff"]}
-          rotation={-75}
-          speed={0.2}
-          scale={0.5}
-          frequency={1}
-          warpStrength={1}
-          mouseInfluence={1}
-          parallax={0.5}
-          noise={0.1}
-          transparent={false}
-        />
+      <div className="bg-span" aria-hidden="true">
+        <div className="bg-span__inner">
+          <ColorBlend
+            colors={["#d12c16ff"]}
+            rotation={-75}
+            speed={0.2}
+            scale={0.5}
+            frequency={1}
+            warpStrength={1}
+            mouseInfluence={1}
+            parallax={0.5}
+            noise={0.1}
+            transparent={false}
+          />
+        </div>
       </div>
+
+      <main className="content">
+        <div className="transparent-box">
+          <h1>Welcome to My Portfolio</h1>
+        </div>
+      </main>
     </>
   );
 }
